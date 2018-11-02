638754cad9673509f9e58e16690e65fd889f44e4
maialam <i.alamxvi@gmail.com>
2018-11-02 11:40:12 +0000

    third commit

---
+++ src/main/java/com/qa/domain/Class.java
@@ -0,0 +1,63 @@
+package com.qa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Class {
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String firstName;
+	private String secondName;
+	private String classRoom;
+	
+	public Account(String firstName, String secondName, String classRoom) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.classRoom = classRoom;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getClassRoom() {
+		return classRoom;
+	}
+
+	public void setClassRoom(String classRoom) {
+		this.classRoom = classRoom;
+	}
+
+	public Class() {
+
+	}
+
+
+	
+
+}
